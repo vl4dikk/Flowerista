5e51b7a82d547335aade0e0e38fe51b54a341822
foxstudent101752 <vladrockyvlad@gmail.com>
2023-10-14 23:39:43 +0300

    added CORS

--- src/main/java/ua/flowerista/shop/controllers/TestController.java
+++ src/main/java/ua/flowerista/shop/controllers/TestController.java
@@ -1,14 +1,16 @@
 package ua.flowerista.shop.controllers;
 
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/api/test")
+@CrossOrigin(origins = "*")
 public class TestController {
 	
 	@GetMapping
