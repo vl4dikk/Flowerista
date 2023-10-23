8e6f60b098e0d8aabc25afd897030e8ffdd3e24d
foxstudent101752 <vladrockyvlad@gmail.com>
2023-10-23 14:54:00 +0300

    added first method to bouquete controller

---
+++ src/main/java/ua/flowerista/shop/controllers/BouqueteController.java
@@ -0,0 +1,35 @@
+package ua.flowerista.shop.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import ua.flowerista.shop.dto.BouqueteDto;
+import ua.flowerista.shop.services.BouqueteService;
+
+@RestController
+@RequestMapping("/api/bouquete")
+@CrossOrigin(origins = "*")
+public class BouqueteController {
+	
+	@Autowired
+	private BouqueteService service;
+	
+	@GetMapping("/bs")
+	@Operation(summary = "Get bestsellers", description = "")
+	public ResponseEntity<CollectionModel<EntityModel<BouqueteDto>>> getBouqueteBestSellers() {
+		List<EntityModel<BouqueteDto>> bouquetesModels = service.getBouquetesBestSellers().stream().map(bouquete -> EntityModel.of(bouquete)).collect(Collectors.toList());
+		CollectionModel<EntityModel<BouqueteDto>> collectionModel = CollectionModel.of(bouquetesModels);
+		return ResponseEntity.ok(collectionModel);
+	}
+
+}
