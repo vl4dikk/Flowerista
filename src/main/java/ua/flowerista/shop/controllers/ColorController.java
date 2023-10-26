3e8d49b70f709ce58329f60a63d9f007a4d26210
foxstudent101752 <vladrockyvlad@gmail.com>
2023-10-26 22:28:16 +0300

    added rest controllers for color and flower entity

---
+++ src/main/java/ua/flowerista/shop/controllers/ColorController.java
@@ -0,0 +1,31 @@
+package ua.flowerista.shop.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import ua.flowerista.shop.dto.ColorDto;
+import ua.flowerista.shop.services.ColorService;
+
+@RestController
+@RequestMapping("/api/color")
+@CrossOrigin(origins = "*")
+public class ColorController {
+	
+	@Autowired
+	ColorService service;
+	
+	@GetMapping
+	@Operation(summary = "Get all collors", description = "Returns list of all colors")
+	public ResponseEntity<List<ColorDto>> getAllCollors() {
+		List<ColorDto> colors = service.getAllColors();
+		return ResponseEntity.ok(colors);
+	}
+
+}
---
+++ src/main/java/ua/flowerista/shop/controllers/FlowerController.java
@@ -0,0 +1,31 @@
+package ua.flowerista.shop.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import ua.flowerista.shop.dto.FlowerDto;
+import ua.flowerista.shop.services.FlowerService;
+
+@RestController
+@RequestMapping("/api/flower")
+@CrossOrigin(origins = "*")
+public class FlowerController {
+	
+	@Autowired
+	FlowerService service;
+	
+	@GetMapping
+	@Operation(summary = "Get all flowers", description = "Returns list of all flowers")
+	public ResponseEntity<List<FlowerDto>> getAllFlowers() {
+		List<FlowerDto> flowers = service.getAllFlowers();
+		return ResponseEntity.ok(flowers);
+	}
+
+}
