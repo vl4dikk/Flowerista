20a45123daaec6dfd8bf09adbcf6ee4ca8bbd783
foxstudent101752 <vladrockyvlad@gmail.com>
2023-12-01 15:27:10 +0200

    added patterns for passwords

--- src/main/java/ua/flowerista/shop/dto/user/UserChangePasswordRequestDto.java
+++ src/main/java/ua/flowerista/shop/dto/user/UserChangePasswordRequestDto.java
@@ -1,5 +1,6 @@
 package ua.flowerista.shop.dto.user;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,10 @@ import lombok.NoArgsConstructor;
 public class UserChangePasswordRequestDto {
 	
     private String currentPassword;
+	@Pattern(
+		    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+		    message = "Password must be at least 8 characters long, contain a mix of uppercase and lowercase letters, include at least one numerical digit, and include at least one special character."
+		)
     private String newPassword;
 
 }
--- src/main/java/ua/flowerista/shop/dto/user/UserPasswordResetDto.java
+++ src/main/java/ua/flowerista/shop/dto/user/UserPasswordResetDto.java
@@ -1,5 +1,6 @@
 package ua.flowerista.shop.dto.user;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +10,17 @@ import lombok.Setter;
 @Setter
 public class UserPasswordResetDto {
 
+	@Pattern(
+		    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+		    message = "Password must be at least 8 characters long, contain a mix of uppercase and lowercase letters, include at least one numerical digit, and include at least one special character."
+		)
 	private String password;
 
 	private String token;
 
+	@Pattern(
+		    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+		    message = "Password must be at least 8 characters long, contain a mix of uppercase and lowercase letters, include at least one numerical digit, and include at least one special character."
+		)
 	private String passwordRepeated;
 }
