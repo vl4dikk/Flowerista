47dbbf73c9428acd3589d1487d0f79b37a6483ee
foxstudent101752 <vladrockyvlad@gmail.com>
2023-12-05 17:17:12 +0200

    fix of auth

--- src/main/java/ua/flowerista/shop/controllers/AuthController.java
+++ src/main/java/ua/flowerista/shop/controllers/AuthController.java
@@ -48,168 +48,139 @@ import ua.flowerista.shop.services.UserService;
 @RestController
 @RequestMapping("/api/auth")
 @CrossOrigin
-@Tag(name="AUTH controller", description = "Operations with sign up and sign in")
+@Tag(name = "AUTH controller", description = "Operations with sign up and sign in")
 public class AuthController {
 
 	@Autowired
 	private UserService service;
-	
+
 	@Autowired
 	private AuthenticationService authService;
-	
-    @Autowired
-    private ApplicationEventPublisher eventPublisher;
-    
-    @Autowired
-    private JavaMailSender mailSender;
-    
-    @Autowired
-    private Environment env;
+
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@Autowired
+	private Environment env;
 
 	@PostMapping(value = "/register", consumes = "application/json")
 	@Operation(summary = "Register new user", description = "Returns bad request if something went wrong, and accepted if everything fine")
-    @ApiResponses(value = {
-    		@ApiResponse(responseCode = "400",
-			description = "If email or phone number already exist"),
-    		@ApiResponse(responseCode = "202",
-			description = "Data was accepted")
-    })
-	public ResponseEntity<?> registerNewUser(@RequestBody @Valid UserRegistrationBodyDto regDto, final HttpServletRequest request) {
-		if(service.existsByEmail(regDto.getEmail())) {
+	@ApiResponses(value = { @ApiResponse(responseCode = "400", description = "If email or phone number already exist"),
+			@ApiResponse(responseCode = "202", description = "Data was accepted") })
+	public ResponseEntity<?> registerNewUser(@RequestBody @Valid UserRegistrationBodyDto regDto,
+			final HttpServletRequest request) {
+		if (service.existsByEmail(regDto.getEmail())) {
 			return ResponseEntity.badRequest().body("Email already exists");
-		}if(service.existsByPhoneNumber(regDto.getPhoneNumber())) {
+		}
+		if (service.existsByPhoneNumber(regDto.getPhoneNumber())) {
 			return ResponseEntity.badRequest().body("Phone number already exists");
 		}
 		final User registered = service.registerNewUserAccount(regDto);
-		eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
+		eventPublisher
+				.publishEvent(new OnRegistrationCompleteEvent(registered, request.getLocale(), getAppUrl(request)));
 		return ResponseEntity.accepted().build();
 	}
-	
+
 	@GetMapping("/checkEmail/{email}")
 	@Operation(summary = "Check if email exists", description = "Returns true - if exists, false - if not")
-	public ResponseEntity<Boolean> existsByEmail (@PathVariable (value = "email") String email){
+	public ResponseEntity<Boolean> existsByEmail(@PathVariable(value = "email") String email) {
 		boolean exists = service.existsByEmail(email);
-		if(exists == true) {
+		if (exists == true) {
 			return ResponseEntity.ok(exists);
 		}
 		return ResponseEntity.ok(exists);
 	}
-	
+
 	@GetMapping("/checkPhone/{phoneNumber}")
 	@Operation(summary = "Check if phone number exists", description = "Returns true - if exists, false - if not")
-	public ResponseEntity<Boolean> existsByPhoneNumber (@PathVariable (value = "phoneNumber") int phoneNumber){
+	public ResponseEntity<Boolean> existsByPhoneNumber(@PathVariable(value = "phoneNumber") int phoneNumber) {
 		boolean exists = service.existsByPhoneNumber(phoneNumber);
-		if(exists == true) {
+		if (exists == true) {
 			return ResponseEntity.ok(exists);
 		}
 		return ResponseEntity.ok(exists);
 	}
-	
-	  @PostMapping("/authenticate")
-	  @Operation(summary = "User login endpoint", description = "Returns refresh and access tokens")
-	    @ApiResponses(value = {
-	    		@ApiResponse(responseCode = "403",
-				description = "If email or password didnt match"),
-	    		@ApiResponse(responseCode = "200",
-				description = "If data was accepted")
-	    })
-	  public ResponseEntity<?> authenticate(
-	      @RequestBody UserLoginBodyDto request
-	  ) {
-		  UserAuthenticationResponseDto response = authService.authenticate(request);
-		  if(response.getAccessToken().equals(null) || response.getRefreshToken().equals(null)) {
-			  return ResponseEntity.badRequest().body("Email or password dont match");
-		  }
-	    return ResponseEntity.ok(response);
-	  }
-	  
-	  @PostMapping("/refresh-token")
-	  @Operation(summary = "Api to refresh token", description = "Returns refreshed tokens")
-	  public void refreshToken(
-	      HttpServletRequest request,
-	      HttpServletResponse response
-	  ) throws IOException {
-	    authService.refreshToken(request, response);
-	  }
-	
+
+	@PostMapping("/authenticate")
+	@Operation(summary = "User login endpoint", description = "Returns refresh and access tokens")
+	@ApiResponses(value = { @ApiResponse(responseCode = "403", description = "If email or password didnt match"),
+			@ApiResponse(responseCode = "200", description = "If data was accepted") })
+	public ResponseEntity<?> authenticate(@RequestBody UserLoginBodyDto request) {
+		UserAuthenticationResponseDto response = authService.authenticate(request);
+		if (response.getAccessToken().equals(null) || response.getRefreshToken().equals(null)) {
+			return ResponseEntity.badRequest().body("Email or password dont match");
+		}
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/refresh-token")
+	@Operation(summary = "Api to refresh token", description = "Returns refreshed tokens")
+	public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		authService.refreshToken(request, response);
+	}
+
 	@PostMapping("/registrationConfirm/{token}")
 	@Operation(summary = "Validating token", description = "If token is expired, deleting token and user")
-    @ApiResponses(value = {
-    		@ApiResponse(responseCode = "400",
-			description = "If token is expired or invalid"),
-    		@ApiResponse(responseCode = "202",
-			description = "Data was accepted")
-    })
-	public ResponseEntity<?> registrationConfirm (@PathVariable (value = "token") String token){
+	@ApiResponses(value = { @ApiResponse(responseCode = "400", description = "If token is expired or invalid"),
+			@ApiResponse(responseCode = "202", description = "Data was accepted") })
+	public ResponseEntity<?> registrationConfirm(@PathVariable(value = "token") String token) {
 		String tokenValidated = service.validateVerificationToken(token);
-		if(tokenValidated.equals("invalidToken")) {
-			 return ResponseEntity.status(HttpStatus.NOT_FOUND).location(URI.create("http://flowerista-frontend.vercel.app/")).build();
-		}if(tokenValidated.equals("expired")) {
-			 return ResponseEntity.status(HttpStatus.BAD_REQUEST).location(URI.create("http://flowerista-frontend.vercel.app/")).build();
+		if (tokenValidated.equals("invalidToken")) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND)
+					.location(URI.create("http://flowerista-frontend.vercel.app/")).build();
 		}
-     return ResponseEntity.status(HttpStatus.ACCEPTED).header(HttpHeaders.LOCATION, "http://flowerista-frontend.vercel.app/login").build();
+		if (tokenValidated.equals("expired")) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.location(URI.create("http://flowerista-frontend.vercel.app/")).build();
+		}
+		return ResponseEntity.status(HttpStatus.ACCEPTED)
+				.header(HttpHeaders.LOCATION, "http://flowerista-frontend.vercel.app/login").build();
 	}
-	
+
 	@PostMapping("/resetPassword")
 	@Operation(summary = "Restoring access api", description = "Looks for user with email, and sends letter on email to restore access")
-	public ResponseEntity<?> resetPassword(HttpServletRequest request, 
-	  @RequestParam("email") String userEmail) {
-	    User user = service.findUserByEmail(userEmail);
-	    if (user == null) {
-	        return ResponseEntity.notFound().build();
-	    }
-	    String token = UUID.randomUUID().toString();
-	    service.createPasswordResetTokenForUser(user, token);
-	    mailSender.send(constructResetTokenEmail("flowerista-frontend.vercel.app", 
-	      request.getLocale(), token, user));
-	    return ResponseEntity.accepted().body("Token was sent");
+	public ResponseEntity<?> resetPassword(HttpServletRequest request, @RequestParam("email") String userEmail) {
+		User user = service.findUserByEmail(userEmail);
+		if (user == null) {
+			return ResponseEntity.notFound().build();
+		}
+		String token = UUID.randomUUID().toString();
+		service.createPasswordResetTokenForUser(user, token);
+		mailSender.send(constructResetTokenEmail("flowerista-frontend.vercel.app", request.getLocale(), token, user));
+		return ResponseEntity.accepted().body("Token was sent");
 	}
-	
-	
+
 	@PostMapping("/changePassword")
 	@Operation(summary = "Restoring password with token", description = "Changing user password with token")
-	public ResponseEntity<?> changePassword (@RequestBody @Valid UserPasswordResetDto dto) {
+	public ResponseEntity<?> changePassword(@RequestBody @Valid UserPasswordResetDto dto) {
 		String response = service.resetPassword(dto);
-		if(response.equals("Password changed")) {
+		if (response.equals("Password changed")) {
 			return ResponseEntity.ok(response);
 		}
 		return ResponseEntity.badRequest().body(response);
 	}
-	
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public Map<String, String> handleValidationExceptions(
-	  MethodArgumentNotValidException ex) {
-	    Map<String, String> errors = new HashMap<>();
-	    ex.getBindingResult().getAllErrors().forEach((error) -> {
-	        String fieldName = ((FieldError) error).getField();
-	        String errorMessage = error.getDefaultMessage();
-	        errors.put(fieldName, errorMessage);
-	    });
-	    return errors;
+
+	private String getAppUrl(HttpServletRequest request) {
+		return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
 	}
-	
-	
-	
-    private String getAppUrl(HttpServletRequest request) {
-        return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
-    }
-    
-    private SimpleMailMessage constructResetTokenEmail(final String contextPath, final Locale locale, final String token, final User user) {
-        final String url = contextPath + "/changePassword?token=" + token;
-        final String message = "To reset your password, follow the link (link is valid only 24 hours)";
-        return constructEmail("Flowerista Reset Password", message + " \r\n" + url, user);
-    }
-    
-    private SimpleMailMessage constructEmail(String subject, String body, User user) {
-        final SimpleMailMessage email = new SimpleMailMessage();
-        email.setSubject(subject);
-        email.setText(body);
-        email.setTo(user.getEmail());
-        email.setFrom(env.getProperty("support.email"));
-        return email;
-    }
-    
-    
+
+	private SimpleMailMessage constructResetTokenEmail(final String contextPath, final Locale locale,
+			final String token, final User user) {
+		final String url = contextPath + "/changePassword?token=" + token;
+		final String message = "To reset your password, follow the link (link is valid only 24 hours)";
+		return constructEmail("Flowerista Reset Password", message + " \r\n" + url, user);
+	}
+
+	private SimpleMailMessage constructEmail(String subject, String body, User user) {
+		final SimpleMailMessage email = new SimpleMailMessage();
+		email.setSubject(subject);
+		email.setText(body);
+		email.setTo(user.getEmail());
+		email.setFrom(env.getProperty("support.email"));
+		return email;
+	}
 
 }
