98fd1f4fa6c95040013c85a4e66f0c6ab8cbb99b
foxstudent101752 <vladrockyvlad@gmail.com>
2023-12-08 14:21:10 +0200

    added cors config headers allows

--- src/main/java/ua/flowerista/shop/configs/CorsConfig.java
+++ src/main/java/ua/flowerista/shop/configs/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedHeaders("Content-Type", "Authorization");
+                .allowedHeaders("Content-Type", "Authorization", "Access-Control-Allow-Headers", "*");
     }
 
 }
