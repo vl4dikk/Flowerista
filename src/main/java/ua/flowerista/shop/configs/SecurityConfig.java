3bc6efecf71f19622bbd19018064c6b2fc354d05
foxstudent101752 <vladrockyvlad@gmail.com>
2023-12-11 21:11:13 +0200

    added cors to spirng security chain

--- src/main/java/ua/flowerista/shop/configs/SecurityConfig.java
+++ src/main/java/ua/flowerista/shop/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package ua.flowerista.shop.configs;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -11,6 +13,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
 import ua.flowerista.shop.models.Role;
@@ -32,7 +37,8 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable())
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+		.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL).permitAll()
 						.requestMatchers("/api/admin/**").hasAuthority(Role.ADMIN.name())
 						.requestMatchers("/api/user/**").hasAnyAuthority(Role.USER.name(), Role.ADMIN.name())
@@ -46,5 +52,16 @@ public class SecurityConfig {
 
 		return http.build();
 	}
+	
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(List.of("https://flowerista-frontend.vercel.app", "http://localhost:3000"));
+        cors.setAllowedMethods(List.of("GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"));
+        cors.setAllowedHeaders(List.of("Content-Type", "Authorization", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cors);
+        return source;
+    }
 
 }
