205cc249c7743713b0741629772f28402becfbbe
foxstudent101752 <vladrockyvlad@gmail.com>
2023-12-14 15:56:47 +0200

    validation fix, added cyrilic letters and hyphens and spaces

--- src/main/java/ua/flowerista/shop/dto/AddressDto.java
+++ src/main/java/ua/flowerista/shop/dto/AddressDto.java
@@ -13,13 +13,13 @@ import lombok.Setter;
 @Setter
 public class AddressDto {
 	@NotBlank(message = "City name must not be empty")
-	@Pattern(regexp = "^\\p{L}*$", message = "Only letters are allowed in the city name")
+	@Pattern(regexp = "^[\\p{L}\\p{IsCyrillic} -]*$", message = "Only letters, spaces, hyphens, and combinations are allowed in the city name")
 	private String city;
 	@NotBlank(message = "Street name must not be empty")
-	@Pattern(regexp = "^\\p{L}*$", message = "Only letters are allowed in the street name")
+	@Pattern(regexp = "^[\\p{L}\\p{IsCyrillic} -]*$", message = "Only letters, spaces, hyphens, and combinations are allowed in the street name")
 	private String street;
 	@NotBlank(message = "House number must not be empty")
-	@Pattern(regexp = "^[a-zA-Z0-9/]+$", message = "Only letters, numbers, '/', and combinations are allowed in the house number")
+	@Pattern(regexp = "^[\\p{IsLatin}\\p{IsCyrillic}0-9/]+$", message = "Only letters, numbers, '/', and combinations are allowed in the house number")
 	private String house;
 	@Pattern(regexp = "^[0-9]*$", message = "Only numbers are allowed in the entrance")
 	private String entrance;
--- src/main/java/ua/flowerista/shop/dto/user/UserRegistrationBodyDto.java
+++ src/main/java/ua/flowerista/shop/dto/user/UserRegistrationBodyDto.java
@@ -18,14 +18,14 @@ public class UserRegistrationBodyDto {
 
 	@Size(min = 2, message = "Firstname is too short")
 	@Size(max = 50, message = "Firstname is too long")
-	@Pattern(regexp = "^\\p{L}*$", message = "Firstname should contain only alphabets")
-	@Pattern(regexp = "[A-Z].*", message = "Firstname must start with an uppercase letter")
+	@Pattern(regexp = "^[\\p{L}-]*$", message = "Firstname should contain letters and hyphens")
+	@Pattern(regexp = "^[A-Z\\p{IsCyrillic}].*", message = "Firstname must start with an uppercase letter")
 	private String firstName;
 
 	@Size(min = 2, message = "Lastname is too short")
 	@Size(max = 50, message = "Lastname is too long")
-	@Pattern(regexp = "^\\p{L}*$", message = "Lastname should contain only alphabets")
-	@Pattern(regexp = "[A-Z].*", message = "Lastname must start with an uppercase letter")
+	@Pattern(regexp = "^[\\p{L}-]*$", message = "Lastname should contain only letters")
+	@Pattern(regexp = "^[A-Z\\p{IsCyrillic}].*", message = "Lastname must start with an uppercase letter")
 	private String lastName;
 	@Email
 	private String email;
